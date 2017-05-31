cd772c78dbace0b524a15d54e46b55c87034e15a
zhanhui913 <zhanhui913@gmail.com>
2017-05-30 20:19:45 -0700

    modify

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Zhan\n');
+  res.end('Hello Zhan this is new\n');
 }).listen(8080, '45.33.42.219');
 console.log('Server running at http://45.33.42.219:8080/');
