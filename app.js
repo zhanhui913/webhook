f5265ccc8d05cd03ea8ddc9a986fb8da509cec6c
zhanhui913 <zhanhui913@gmail.com>
2017-05-30 20:46:33 -0700

    More changes to ip and port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
+var ip = 45.33.42.219;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -12,8 +13,10 @@ app.get('/', function(req, res){
     res.send('Welcome to my linode server!');
 });
 
-app.listen(port, function(){
-    console.log('NodeJS is running my app on PORT: ' + port);
+app.listen(port, ip, function(){
+	var host = server.address().address;
+  	var port = server.address().port;
+  	console.log('Example app listening at http://%s:%s', host, port);
 });
 
 module.exports = app;
