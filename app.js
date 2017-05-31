10f9c4a5342ea153321aea7a98538511525811be
zhanhui913 <zhanhui913@gmail.com>
2017-05-30 20:59:04 -0700

    more changes to app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
-var ip = '45.33.42.219';
+var ip = 'localhost';
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
