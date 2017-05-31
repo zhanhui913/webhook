842693205878803ac3ff63e8aff966095c1689d2
zhanhui913 <zhanhui913@gmail.com>
2017-05-30 20:53:08 -0700

    Change ip back to local as we have nginx for reverse proxy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
-var ip = '45.33.42.219';
+var ip = '127.0.0.1';
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.get('/', function(req, res){
     res.send('Welcome to my linode server!');
 });
 
-app.listen(port, ip, function(){
+var server = app.listen(port, ip, function(){
 	var host = server.address().address;
   	var port = server.address().port;
   	console.log('Example app listening at http://%s:%s', host, port);
