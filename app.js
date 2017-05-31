867fffb2766d641f24c9e5b57738b438bb8b2d65
zhanhui913 <zhanhui913@gmail.com>
2017-05-30 20:35:38 -0700

    change port from 3000 to 8080

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
